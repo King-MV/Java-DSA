d864fef5863df54d5b7f0d5d33dcb26cc25e159b
Mridul Vasudeva <88312870+Mridul1703@users.noreply.github.com>
2022-01-27 11:47:16 +0530

    Update SearchingInList.c

--- SearchingInList.c
+++ SearchingInList.c
@@ -1,3 +1,4 @@
+// A C program used to find an element inside a list and return its address if found..
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,6 +8,7 @@ struct node {
 };
 struct node* head;
 
+// Function used to insert elements..
 void Insert(int value) {
     struct node *temp = (struct node*) malloc(sizeof(struct node));
     temp->data = value;
@@ -14,6 +16,7 @@ void Insert(int value) {
     head = temp;
 }
 
+// Function used to take input of elements..
 void Input(int n) {
     int x;
     for (int i=0; i<n; i++) {
@@ -23,19 +26,24 @@ void Input(int n) {
     }
 }
 
+// Function used to search for the element..
 void Search() {
     int key , count = 0;
     printf("\nEnter element to be searched: ");
     scanf("%d",&key);
     while(head!=NULL) {
         if (head->data == key) {
+            // Returns the address if key is found in the list..            
             printf("\nAddress of %d is %d\n",key,head);
+            // Count used to find wheter element is found or not..            
             count++;
             break;
         } else {
+            // This keeps changing the address to which head is pointing..            
             head = head->next;
         }
     }
+    // If element not found inside list this will be executed..     
     if(count==0) {
         printf("\nElement not found\n");
     }
