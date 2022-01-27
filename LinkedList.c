9448163c665ea49ef7800f1edd7bf1e194ef9d4c
Mridul Vasudeva <88312870+Mridul1703@users.noreply.github.com>
2022-01-27 10:56:53 +0530

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -1,19 +1,27 @@
+// A C program used to Create , Insert and Display a Linked List...
 #include <stdio.h>
 #include <stdlib.h>
 
+// Node structure created.
 struct node {
     int data;
     struct node *next;
 };
 struct node* head;
 
+// Insertion function()
 void Insert(int value) {
+    // New node temp is created here everytime for insertion...    
     struct node *temp = (struct node*) malloc(sizeof(struct node));
+    // User input value is stored inside new node...    
     temp->data = value;
+    // head is now pointing to the next node of the inserted node...    
     temp->next = head; 
+    // Value of temp is stored inside head..    
     head = temp;
 }
 
+// Input function()
 void Input(int n) {
     int x;
     for (int i=0; i<n; i++) {
@@ -23,7 +31,8 @@ void Input(int n) {
     }
 }
 
-void Print() {
+// Print function()
+void Print() {    
     struct node *temp = head;
     while(temp!=NULL) {
         printf("%d ",temp->data);
